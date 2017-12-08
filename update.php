542d27c78158a51fc0ca136ef92b3cb4d70d256d
aikuaivenchorr <inarilainen@gmail.com>
2017-12-08 12:18:57 +0100

    update

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+
+
+if(isset($_POST['publish'])){
+    require DIRBASE . 'database/db.php';
+    
+   $sql = "UPDATE `blogPosts` SET `postDate`=NOW(), `postTitle`=:headline,`postText`=:postText, `categoryName`=:categoryName, `imageName`=:imageName WHERE `postID`='$postID'";
+    
+    $statement = $pdo -> prepare($sql);
+    
+    $statement->bindValue(':headline', $_POST['headline'], PDO::PARAM_STR);
+    $statement->bindValue(':postText', $_POST['postText'], PDO::PARAM_STR);
+    $statement->bindValue(':categoryName', $_POST['categoryName']);
+    $statement->bindValue(':imageName', $_FILES['upload']['name']);
+    $statement->execute();
+    
+    if($statement){
+        $success = true; 
+        
+    }
+    $pdo = null;
+
+
+}
+
